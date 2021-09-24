d6c5190bad8e099e9d83ce67f135b6e31e95893e
Tribhi <tribhi@umich.edu>
2021-09-24 00:54:49 -0400

    Added gitignore

---
+++ src/ros_wrapper.h
@@ -0,0 +1,38 @@
+#ifndef ros_wrapper
+#define ros_wrapper
+// #include <hssh/metrical/data_queue.h>
+// #include <hssh/metrical/data.h>
+// #include <laser/line_extraction.h>
+// #include <laser/line_extractor_params.h>
+#include <core/imu_data.h>
+#include <core/odometry.h>
+// #include <core/laser_scan.h>
+#include <core/motion_state.h>
+#include <system/module_communicator.h>
+// #include <utils/algorithm_ext.h>
+// #include <utils/auto_mutex.h>
+#include <utils/timestamp.h>
+#include <cassert>
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+
+
+namespace vulcan
+{
+namespace system { class ModuleCommunicator; }
+namespace hssh
+{
+
+class ros_wrapping
+{
+public:
+ros::NodeHandle nh_;
+ros::Subscriber sub_;
+
+ros_wrapping()
+
+}
+}
+}               
+
+#endif // HSSH_UTILS_METRICAL_DATA_QUEUE_H
